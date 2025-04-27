cdb97b6cc957b70d8cb60ec4c33c1f2c454c7364
Saandeep Kotte <saandeepkotte5@gmail.com>
2025-04-27 15:35:39 +0530

    project-setup(configuration): setting up the project with required configuration

---
+++ src/test/java/com/saandeep/govrn/GovrnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saandeep.govrn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovrnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
