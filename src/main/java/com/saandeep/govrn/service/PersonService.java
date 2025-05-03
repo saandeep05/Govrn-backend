af05ffddfcb0ee1c94a5837dd2d5ad146d3bb973
Saandeep Kotte <saandeepkotte5@gmail.com>
2025-05-03 22:44:40 +0530

    fix(user role): this commit handles user role default case

--- src/main/java/com/saandeep/govrn/service/PersonService.java
+++ src/main/java/com/saandeep/govrn/service/PersonService.java
@@ -4,6 +4,7 @@ import com.saandeep.govrn.dto.PersonDTO;
 import com.saandeep.govrn.model.Person;
 import com.saandeep.govrn.model.Region;
 import com.saandeep.govrn.repository.PersonRepository;
+import com.saandeep.govrn.util.enums.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,11 +23,12 @@ public class PersonService {
 
     public Person createPerson(PersonDTO personDto) {
         Region region = regionService.getRegion(personDto.getRegionId());
+        UserRole role = personDto.getRole();
         Person person = new Person(null,
                 personDto.getAadhaarNumber(),
                 personDto.getEmail(),
                 personDto.getPassword(),
-                personDto.getRole(),
+                role != null ? role : UserRole.CITIZEN,
                 true,
                 region,
                 null
