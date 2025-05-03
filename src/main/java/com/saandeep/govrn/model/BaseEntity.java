39e2d1abb1d4b029054918d9d20b5590dd44ee2c
Saandeep Kotte <saandeepkotte5@gmail.com>
2025-05-03 23:14:03 +0530

    fix(BaseEntity): this commit makes fields, in BaseEntity, protected

--- src/main/java/com/saandeep/govrn/model/BaseEntity.java
+++ src/main/java/com/saandeep/govrn/model/BaseEntity.java
@@ -1,20 +1,19 @@
 package com.saandeep.govrn.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
+import jakarta.persistence.*;
+import lombok.Data;
 import org.hibernate.envers.Audited;
 
 import java.time.LocalDateTime;
 
 @Audited
 @MappedSuperclass
+@Data
 public abstract class BaseEntity {
     @Column(name = "created_at", updatable = false)
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private LocalDateTime deletedAt;
+    protected LocalDateTime createdAt;
+    protected LocalDateTime updatedAt;
+    protected LocalDateTime deletedAt;
 
     @PrePersist
     public void prePersist() {
